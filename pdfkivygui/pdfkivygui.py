e01f933d919f7ffbf59e31e87a042cc979914ba6
pythonpanda <jefpadfi@gmail.com>
2020-08-07 20:52:30 -0600

    Initial Commit

--- pdfkivygui/pdfkivygui.py
+++ pdfkivygui/pdfkivygui.py
@@ -5,11 +5,25 @@ Date: 08/07/2019
 Version: 1
 """
 
+import os
+
 import kivy
-from kivy.uix.widget import Widget
 from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy.event import EventDispatcher
 from kivy.properties import BooleanProperty, ObjectProperty, NumericProperty, StringProperty, ListProperty
 
-# import matplotlib and have it use matplotlib.backend_kivy for its system
 import matplotlib
+import matplotlib.pyplot as plt
+
+from collections import OrderedDict
+
+import numpy as np
+import pandas as pd
+
+
+# lets make sure matplotlib knows to use our kivy backend from https://github.com/kivy-garden/garden.matplotlib
+matplotlib.use("module://pdfkivygui.gard.matplotlib.backend_kivy")
+
+
 
